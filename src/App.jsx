02167b9d6157b45aa54384670c2eb67430dc7496
Imran <imranmansuri038@gmail.com>
2023-02-02 15:16:21 +0530

    api setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 
 const App = () => {
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
   return <div>App</div>;
 };
 
