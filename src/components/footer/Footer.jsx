77f5eacaafe961a0cfdc0fbe0ba24b473645c28c
Imran <imranmansuri038@gmail.com>
2023-02-04 16:52:07 +0530

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import "./style.scss";
+
+const footerData = [
+  {
+    title: "useful links",
+    links: [
+      { item: "Terms Of Use" },
+      { item: "Privacy-Policy" },
+      { item: "About" },
+      { item: "Blog" },
+      { item: "FAQ" },
+    ],
+  },
+  {
+    title: "social links",
+    links: [
+      { icon: <FaFacebookF /> },
+      { icon: <FaInstagram /> },
+      { icon: <FaTwitter /> },
+      { icon: <FaLinkedin /> },
+    ],
+  },
+];
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <ContentWrapper>
+        <div className="top">
+          <ul className="menuItems">
+            {footerData[0].links.map((curItem, idx) => {
+              return (
+                <li key={idx} className="menuItem">
+                  {curItem.item}
+                </li>
+              );
+            })}
+          </ul>
+          <div className="socialIcons">
+            {footerData[1].links.map((curItem, idx) => {
+              return (
+                <span key={idx} className="icon">
+                  {curItem.icon}
+                </span>
+              );
+            })}
+          </div>
+        </div>
 
-export default Footer
+        <div className="bottom">
+          <p className="copyright">
+            © {new Date().getFullYear()} <span>Shaad Imran</span>. All Rights
+            Reserved
+          </p>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
