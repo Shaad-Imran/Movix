795fa5a00dbd3f50e916629d236f57f45b08ce5f
Imran <imranmansuri038@gmail.com>
2023-02-07 14:14:28 +0530

    details page added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./style.scss";
+
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
 
-export default Details
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+    </div>
+  );
+};
+
+export default Details;
